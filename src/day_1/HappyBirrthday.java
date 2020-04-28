c64bedc2b43420e3dd20cef64eefa9306b67d706
Firuz Khalimov <fkhalimov22@gmail.com>
2020-04-28 15:54:07 -0400

    update

--- src/day_1/HappyBirrthday.java
+++ src/day_1/HappyBirrthday.java
@@ -5,7 +5,7 @@ public class HappyBirrthday {
     public static void main(String[] args) {
 
         System.out.println("Happy Birthday Polina");
-        System.out.println("Thank you")
-            System.out.println("Thank you")
+        System.out.println("Thank you");
+            System.out.println("Thank you");
     }
 }
